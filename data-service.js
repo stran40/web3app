21d0ed8c954336721776c2ec0d31f3c7ae56469a
Steph Tran <stran40@myseneca.ca>
2017-06-09 15:51:00 -0400

    Moved resole in initialize()

--- data-service.js
+++ data-service.js
@@ -2,24 +2,23 @@ const fs = require("fs");
 const chalk = require("chalk");
 var test = chalk.cyan;
 var test2 = chalk.yellow;
-const employees = [];
-const departments = [];
+var error = chalk.red.bold;
 
-// module.exports.employees = [];
-// module.exports.departments = [];
+var employees = [];
+var departments = [];
 
-module.exports.setMessage = (msg) => {
-    return new Promise((resolve, reject)=>{
+setMessage = (msg) => {
+    return new Promise((resolve, reject) => {
         message = msg;
         resolve;
     });
 };
 
-module.exports.getMessage = () => {
-    return new Promise((resolve,reject)=>{
-        if(message.length > 0){
+getMessage = () => {
+    return new Promise((resolve, reject) => {
+        if (message.length > 0) {
             resolve(message)
-        }else{
+        } else {
             reject("Oh No!");
         }
     });
@@ -29,10 +28,10 @@ module.exports.getMessage = () => {
  * 
  * Function constructor for person object
  * --------------------------------*/
-function worker(setNum, setFirstName,setLastName,setEmail,setSSN,
-                setAddressStreet,setAddressCity,setAddressState,
-                setAddressPostal,setMaritalStatus,setManager,
-                setEmployeeManagerNum,setStatus,setDepartment,setHireDate){
+function worker(setNum, setFirstName, setLastName, setEmail, setSSN,
+    setAddressStreet, setAddressCity, setAddressState,
+    setAddressPostal, setMaritalStatus, setManager,
+    setEmployeeManagerNum, setStatus, setDepartment, setHireDate) {
     this.employeeNum = setNum;
     this.firstName = setFirstName;
     this.lastName = setLastName;
@@ -48,40 +47,110 @@ function worker(setNum, setFirstName,setLastName,setEmail,setSSN,
     this.status = setStatus;
     this.department = setDepartment;
     this.hireDate = setHireDate;
- };
+};
 
 // worker prototype: methods setting properties of worker obj
-worker.prototype.setNum = function(newNum){this.employeeNum = newNum};
-worker.prototype.setFirstName = function(newFirstName){this.firstName = newFirstName};
-worker.prototype.setLastName  = function(newLastName){this.lastName = newLastName};
-worker.prototype.setEmail  = function(newEmail){this.email = newEmail};
-worker.prototype.setSSN  = function(newSSN){this.SSN= newSSN};
-worker.prototype.setAddressStreet  = function(newAddressStreet){this.addressStreet = newAddressStreet};
-worker.prototype.setAddressCity  = function(newCity){this.addresCity = newCity};
-worker.prototype.setAddressState  = function(newState){ this.addressState = newState };
-worker.prototype.setAddressPostal  = function(newPostal){this.addressPostal = newPostal};
-worker.prototype.setMaritalStatus  = function(newMarital){this.maritalStatus = newMartial};
-worker.prototype.setManager  = function(newIsManager){this.isManager = newIsManager};
-worker.prototype.setEmployeeManagerNum  = function(newEmployeeManagerNum)
-                                            {this.employeeManagerNum = newEmployeeManagerNum};
-worker.prototype.setStatus  = function(newStatus){this.status = newStatus};
-worker.prototype.setDepartment  = function(newDepart){this.department = newDepart};
-worker.prototype.setHireDate  = function(newHireDate){this.hireDate = newHireDate};
+worker.prototype.setNum = function (newNum) {
+    this.employeeNum = newNum
+};
+worker.prototype.setFirstName = function (newFirstName) {
+    this.firstName = newFirstName
+};
+worker.prototype.setLastName = function (newLastName) {
+    this.lastName = newLastName
+};
+worker.prototype.setEmail = function (newEmail) {
+    this.email = newEmail
+};
+worker.prototype.setSSN = function (newSSN) {
+    this.SSN = newSSN
+};
+worker.prototype.setAddressStreet = function (newAddressStreet) {
+    this.addressStreet = newAddressStreet
+};
+worker.prototype.setAddressCity = function (newCity) {
+    this.addresCity = newCity
+};
+worker.prototype.setAddressState = function (newState) {
+    this.addressState = newState
+};
+worker.prototype.setAddressPostal = function (newPostal) {
+    this.addressPostal = newPostal
+};
+worker.prototype.setMaritalStatus = function (newMarital) {
+    this.maritalStatus = newMartial
+};
+worker.prototype.setManager = function (newIsManager) {
+    this.isManager = newIsManager
+};
+worker.prototype.setEmployeeManagerNum = function (newEmployeeManagerNum) {
+    this.employeeManagerNum = newEmployeeManagerNum
+};
+worker.prototype.setStatus = function (newStatus) {
+    this.status = newStatus
+};
+worker.prototype.setDepartment = function (newDepart) {
+    this.department = newDepart
+};
+worker.prototype.setHireDate = function (newHireDate) {
+    this.hireDate = newHireDate
+};
 
 /*-----------------------------------
  * departObj();
  * 
  * Function constructor for department object
  * --------------------------------*/
-function departObj(setID, setName){
+function departObj(setID, setName) {
     this.departID = setID;
     this.departName = setName;
- };
+};
 
 // department prototype: methods setting properties of department obj
-departObj.prototype.setID = function(newID){this.departID = newID};
-departObj.prototype.setName = function(newName){this.departName = newName};
+departObj.prototype.setID = function (newID) {
+    this.departID = newID
+};
+departObj.prototype.setName = function (newName) {
+    this.departName = newName
+};
 
+function readEmployees() {
+    console.log(test("2. readEmployees() starts."));
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./data/employees.json', 'utf8', function (err, data) {
+            if (err) throw err;
+            obj = JSON.parse(data);
+            for (var i = 0; i < obj.length; i++) {
+                var person = new worker(obj[i].employeeNum, obj[i].firstName,
+                    obj[i].last_name, obj[i].email,
+                    obj[i].SSN, obj[i].addressStreet,
+                    obj[i].addresCity, obj[i].addressState,
+                    obj[i].addressPostal, obj[i].maritalStatus,
+                    obj[i].isManager, obj[i].employeeManagerNum,
+                    obj[i].status, obj[i].department,
+                    obj[i].hireDate);
+                employees.push(person);
+            }
+            resolve(employees);
+        });
+    });
+}; // end of readEmployees()
+
+function readDepartments(msg) {
+    console.log(test("3. (116) readDepartments starts."));
+    var tempArray = [];
+    return new Promise(function (resolve, reject) {
+        fs.readFile('./data/departments.json', 'utf8', function (err, data) {
+            if (err) throw err;
+            obj = JSON.parse(data);
+            for (var i = 0; i < obj.length; i++) {
+                var tempDepartObj = new departObj(obj[i].departmentId, obj[i].departmentName);
+                departments.push(tempDepartObj);
+            }
+            resolve(departments);
+        });
+    });
+} // end of readDepartments();
 
 /*-----------------------------------
  * initialize();
@@ -90,67 +159,32 @@ departObj.prototype.setName = function(newName){this.departName = newName};
  * Converts file's contents into array of objects.
  * Assigns this array to employees array.
  * --------------------------------*/
-module.exports.initialize = () => {
-console.log(test("1. (91) initialize();"));
-
-    return new Promise(function(resolve, reject){
-        function readEmployees(){
-            console.log(test("2. (95). readEmployees();"));
-        return new Promise(function(resolve, reject){
-            fs.readFile('./data/employees.json', 'utf8', function(err, data) {
-            if (err) throw err;
-            obj = JSON.parse(data);
-            for (var i = 0; i < obj.length; i++){
-                var person = new worker(obj[i].employeeNum, obj[i].firstName,
-                                            obj[i].last_name,obj[i].email,
-                                            obj[i].SSN,obj[i].addressStreet,
-                                            obj[i].addresCity,obj[i].addressState,
-                                            obj[i].addressPostal,obj[i].maritalStatus,
-                                            obj[i].isManager,obj[i].employeeManagerNum,
-                                            obj[i].status, obj[i].department,
-                                            obj[i].hireDate);
-                        employees.push(person);
-                }
-                resolve();
-                });
-            });
-        }; // end of readEmployees()
-
-        function readDepartments(msg){
-            console.log(test("3. (121) readDepartments();"));
-            var tempArray= [];
-            return new Promise(function(resolve, reject){
-                    fs.readFile('./data/departments.json', 'utf8', function(err, data) {
-                    if (err) throw err;
-                    obj = JSON.parse(data);
-                    for (var i = 0; i < obj.length; i++){
-                        var tempDepartObj = new departObj(obj[i].departmentId, obj[i].departmentName);
-                        departments.push(tempDepartObj);
-                }
-                    resolve(departments);
-                });
-            });
-        } // end of readDepartments();
+initialize = () => {
+    console.log(test("1. (132) initialize() starts."));
+    return new Promise(function (resolve, reject) {
         // invoke functions in order
         readEmployees()
-        .then(readDepartments)
-        .catch(function(rejectMsg){
-            // catch errors here
-            console.log(rejectMsg);
-        });
-   resolve();
-    
+            .then(readDepartments)
+            .then(() => {
+                resolve();
+            })
+            .catch(function (rejectMsg) {
+                // catch errors here
+                console.log(rejectMsg);
+            });
+            
     });
+  
 }; // end of initialize();
 
 /*-----------------------------------
  * getAllEmployees();
  * --------------------------------*/
-module.exports.getAllEmployees = (msg) => {
-    return new Promise(function(resolve, reject){
-        if(employees.length > 0){
+getAllEmployees = (msg) => {
+    return new Promise(function (resolve, reject) {
+        if (employees.length > 0) {
             resolve(employees);
-        }else{
+        } else {
             reject("No results returned from getAllEmployees();");
         }
     });
@@ -159,19 +193,19 @@ module.exports.getAllEmployees = (msg) => {
 /*-----------------------------------
  * getEmployeesByStatus(status);
  * --------------------------------*/
-module.exports.getEmployeesByStatus = (status) => {
+getEmployeesByStatus = (status) => {
     // create variable to hold matches
     var matches = [];
-    return new Promise(function(resolve, reject){
-        if(employees.length > 0){
-            for (var i = 0; i < employees.length; i++ ){
-                if (status == employees[i].status){
+    return new Promise(function (resolve, reject) {
+        if (employees.length > 0) {
+            for (var i = 0; i < employees.length; i++) {
+                if (status == employees[i].status) {
                     matches.push(employees[i]);
                 }
             }
             resolve(matches);
-        }else{
-            reject("No results returned from getEmployeesByStatus();");
+        } else {
+            reject("\nNo results returned from getEmployeesByStatus();");
         }
     });
 
@@ -180,20 +214,31 @@ module.exports.getEmployeesByStatus = (status) => {
 /*-----------------------------------
  * getDepartments()
  * --------------------------------*/
-module.exports.getDepartments = () => {
+getDepartments = () => {
     // create variable to hold matches
     var matches = [];
-    return new Promise(function(resolve, reject){
-        if(departments.length > 0){
-            for (var i = 0; i < employees.length; i++ ){
-               matches[i].push(departments[i]);
+    console.log(test("(184) getDepartments() : " + JSON.stringify(departments[1])));
+
+    return new Promise(function (resolve, reject) {
+        if (departments.length > 0) {
+            for (var i = 0; i < departments.length; i++) {
+                matches[i].push(departments[i]);
             }
             console.log("194 --- " + test(JSON.stringify(matches)));
-            resolve(JSON.stringify(matches));
+            resolve(matches);
 
-        }else{
-            reject("No results returned from getEmployeesByStatus();");
+        } else {
+            reject("No results returned from getDepartments();");
         }
     });
 
 };
+
+module.exports = {
+    initialize: initialize,
+    setMessage: setMessage,
+    getMessage: getMessage,
+    getAllEmployees: getAllEmployees,
+    getEmployeesByStatus: getEmployeesByStatus,
+    getDepartments: getDepartments
+}
--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 /*********************************************************************************
-* WEB322 – Assignment 03
-* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
-* of this assignment has been copied manually or electronically from any other source
-* (including 3rd party web sites) or distributed to other students.
-*
-* Name: Stephanie Tran Student ID: 132233162 Date: June 8 2017
-*
-* Online (Heroku) Link:https://polar-eyrie-23988.herokuapp.com/
-*
-********************************************************************************/
+ * WEB322 – Assignment 03
+ * I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part
+ * of this assignment has been copied manually or electronically from any other source
+ * (including 3rd party web sites) or distributed to other students.
+ *
+ * Name: Stephanie Tran Student ID: 132233162 Date: June 8 2017
+ *
+ * Online (Heroku) Link:https://polar-eyrie-23988.herokuapp.com/
+ *
+ ********************************************************************************/
 const express = require("express");
 const app = express();
 const path = require("path");
@@ -22,64 +22,78 @@ const HTTP_PORT = process.env.PORT || 8080;
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT + "\n" );
+    console.log("Express http server listening on: " + HTTP_PORT + "\n");
 }
 
 //setup static folder
-app.use(express.static("public")); 
+app.use(express.static("public"));
 
 // setup a 'route' to listen on the default url path (http://localhost)
-app.get("/", function(req,res){
-   res.sendFile(path.join(__dirname + "/views/home.html"));
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname + "/views/home.html"));
 });
 
 // setup another route to listen on /about
-app.get("/about", function(req,res){
-  res.sendFile(path.join(__dirname + "/views/about.html"));
+app.get("/about", function (req, res) {
+    res.sendFile(path.join(__dirname + "/views/about.html"));
 });
 
 // setup route to listen on /employees
-app.get("/employees", (req,res) => {
-  if(req.query.status){
-      res.header("Content-Type", "application/json; charset=utf-8");
-        res.json({message: req.query.status});
-    }else if(req.query.manager){
-        res.json({message: req.query.manager});
-    }else if(req.query.department){
-        res.json({message: req.query.department});
-    }else{
-        dataService.getMessage().then((dataMessage)=>{
-            res.json({message: dataMessage});
-        }).catch((errorMessage)=>{
-            res.json({message: errorMessage});
+app.get("/employees", (req, res) => {
+    if (req.query.status) {
+        res.json({
+            message: req.query.status
+        });
+    } else if (req.query.manager) {
+        res.json({
+            message: req.query.manager
+        });
+    } else if (req.query.department) {
+        res.json({
+            message: req.query.department
+        });
+    } else {
+        dataService.getMessage().then((dataMessage) => {
+            res.json({
+                message: dataMessage
+            });
+        }).catch((errorMessage) => {
+            res.json({
+                message: errorMessage
+            });
         });
     }
 });
 
 // setup route to listen on /departments
-app.get("/departments", (req,res) => {
-    res.header("Content-Type", "application/json; charset=utf-8");
-    res.json({message: dataService.getDepartments});
+app.get("/departments", (req, res) => {
+    res.json({
+        message: dataService.getDepartments
+    });
 });
 
-app.get("/employee/:empNum", (req,res) => {
-    res.json({message: req.params.empNum});
+app.get("/employee/:empNum", (req, res) => {
+    res.json({
+        message: req.params.empNum
+    });
 });
 
 //Function to handle 404 requests to pages that are not found.
 app.use((req, res) => {
-  res.status(404).send("Page Not Found");
+    res.status(404).send("Page Not Found");
 });
 
-console.log("===== START ======\n")
+// Testing 
+console.log(chalk.bgMagenta("\n=================== START ========================\n=================================================="));
+
 dataService.initialize()
-.then(console.log(test("\n** initialize() complete ***\n")))
-.then(() => {app.listen(HTTP_PORT, onHttpStart)})
-.then(() => {
-    console.log(test2("(server.js 79) Running departments(); : "));
-    dataService.getDepartments();
-})
-.catch(function(rejectMsg){
-    // catch any errors here
-    console.log(rejectMsg);
-});
+    .then(() => {
+        app.listen(HTTP_PORT, onHttpStart)
+    })
+    .then(() => {
+       
+    })
+    .catch(function (rejectMsg) {
+        // catch any errors here
+        console.log(rejectMsg);
+    });
