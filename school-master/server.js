a86ae0c384787a84a1f0ac86ef84a56c5b2dd0dc
Stephanie Tran <stran40@myseneca.ca>
2017-07-07 15:09:56 -0400

    starting

--- school-master/server.js
+++ school-master/server.js
@@ -4,7 +4,7 @@
  * of this assignment has been copied manually or electronically from any other source
  * (including 3rd party web sites) or distributed to other students.
  *
- * Name: Stephanie Tran Student ID: 132233162 Date: July 6 2017
+ * Name: Stephanie Tran Student ID: 132233162 Date: July 9 2017
  *
  * Online (Heroku) Link:https://polar-eyrie-23988.herokuapp.com/
  *
